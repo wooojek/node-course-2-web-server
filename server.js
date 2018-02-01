97d82572b64329fba79c58299ef8eaa7a2654a09
wooojek <yokomura123@gmail.com>
2018-02-01 21:10:56 +0100

    added projects site

--- server.js
+++ server.js
@@ -57,6 +57,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+   res.render('projects.hbs', {
+       pageTitle: 'Projects Page',
+       projectsMessage: 'Wow such projects here!'
+   })
+});
+
 app.get('/bad', (req, res) => {
    res.send({
       errorMessage: 'Unable to handle request'
